b3ff68e5b6c585d3dd0acf6aad411b7b9dd8c51d
Avinash Sridhar <sridharavinash@github.com>
2017-05-12 22:08:06 -0400

    refactor to remove gclient stuff to a libs folder

--- app.py
+++ app.py
@@ -4,22 +4,13 @@ from flask import request, jsonify
 import argparse
 import itertools
 
-from apiclient.discovery import build
-from oauth2client.service_account import ServiceAccountCredentials
 
-import httplib2
-from oauth2client import client
-from oauth2client import file
-from oauth2client import tools
-
-import datetime
 import dateutil.parser
 import os
 from flask_sqlalchemy import SQLAlchemy
+from libs import gcal_client
 
 
-CALENDAR_ID = os.environ.get('CALENDAR_ID')
-GCLIENT_DATA = os.environ.get('GCLIENT_DATA')
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 app = Flask(__name__)
@@ -37,53 +28,10 @@ class gevent:
         self.players = []
         self.count = 0
 
-def get_service(api_name, api_version, scope, key_file_location):
-  """Get a service that communicates to a Google API.
-
-  Args:
-    api_name: The name of the api to connect to.
-    api_version: The api version to connect to.
-    scope: A list auth scopes to authorize for the application.
-    key_file_location: The path to a valid service account p12 key file.
-
-  Returns:
-    A service that is connected to the specified API.
-  """
-  credentials = ServiceAccountCredentials.from_json_keyfile_name(key_file_location, scopes=scope)
-  http = credentials.authorize(httplib2.Http())
-
-  # Build the service object.
-  service = build(api_name, api_version, http=http)
-
-  return service
 
 @app.route('/')
 def index():
-    scope = ['https://www.googleapis.com/auth/calendar.readonly','https://www.googleapis.com/auth/calendar']
-
-    key_file_location = 'client_secret.json'
-
-    # Write out the json string to file
-    # since we're hosting it on heroku we need a way
-    # to pass the client secrets to the service creation.
-    client_json = open(key_file_location, 'w')
-    client_json.write(GCLIENT_DATA)
-    client_json.write("\n")
-    client_json.close();
-
-    # Authenticate and construct service.
-    service = get_service('calendar', 'v3', scope, key_file_location)
-
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-
-    eventsResult = service.events().list(
-        calendarId=CALENDAR_ID,
-        timeMin=now,
-        maxResults=1,
-        singleEvents=True,
-        orderBy='startTime').execute()
-
-    events = eventsResult.get('items', [])
+    events = gcal_client.get_cal_details()
     gevents=[]
     for event in events:
         ge = gevent(event)
---
+++ libs/gcal_client.py
@@ -0,0 +1,60 @@
+from apiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+
+import httplib2
+from oauth2client import client
+from oauth2client import file
+from oauth2client import tools
+import os
+import datetime
+
+CALENDAR_ID = os.environ.get('CALENDAR_ID')
+GCLIENT_DATA = os.environ.get('GCLIENT_DATA')
+
+def get_service(api_name, api_version, scope, key_file_location):
+  """Get a service that communicates to a Google API.
+
+  Args:
+    api_name: The name of the api to connect to.
+    api_version: The api version to connect to.
+    scope: A list auth scopes to authorize for the application.
+    key_file_location: The path to a valid service account p12 key file.
+
+  Returns:
+    A service that is connected to the specified API.
+  """
+  credentials = ServiceAccountCredentials.from_json_keyfile_name(key_file_location, scopes=scope)
+  http = credentials.authorize(httplib2.Http())
+
+  # Build the service object.
+  service = build(api_name, api_version, http=http)
+
+  return service
+
+def get_cal_details():
+    scope = ['https://www.googleapis.com/auth/calendar.readonly','https://www.googleapis.com/auth/calendar']
+
+    key_file_location = 'client_secret.json'
+
+    # Write out the json string to file
+    # since we're hosting it on heroku we need a way
+    # to pass the client secrets to the service creation.
+    client_json = open(key_file_location, 'w')
+    client_json.write(GCLIENT_DATA)
+    client_json.write("\n")
+    client_json.close();
+
+    # Authenticate and construct service.
+    service = get_service('calendar', 'v3', scope, key_file_location)
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+
+    eventsResult = service.events().list(
+        calendarId=CALENDAR_ID,
+        timeMin=now,
+        maxResults=1,
+        singleEvents=True,
+        orderBy='startTime').execute()
+
+    events = eventsResult.get('items', [])
+    return events
